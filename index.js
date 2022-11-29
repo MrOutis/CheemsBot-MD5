b603395d74725a0b075820632853a135b594486e
MrOutis <108561034+MrOutis@users.noreply.github.com>
2022-11-29 13:01:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function startXeonBotInc() {
     const XeonBotInc = XeonBotIncConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Subscribe Xeon','Safari','1.0.0'],
+        browser: ['HRK R⊕ĈҚЄ† ∀5','Safari','1.0.0'],
         auth: state
     })
 
